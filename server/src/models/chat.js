66c0f0e00b894ad4b3d187527c6e4c2db7277e4c
purplesheep513 <72682862+purplesheep513@users.noreply.github.com>
2020-11-16 22:22:47 +0900

    Update chat.js

--- server/src/models/chat.js
+++ server/src/models/chat.js
@@ -3,7 +3,7 @@ import mongoose, { Schema } from 'mongoose';
 var chatSchema = Schema({
     idx : mongoose.Schema.Types.ObjectId,
     date : {type : Date, default:Date.now, required : true},
-    roomname: String,
+    sender: String,
     username: String,
     message: String,
     users: [String]

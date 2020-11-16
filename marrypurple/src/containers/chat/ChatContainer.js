46b1a69316c5ff0620ec0fff57040138708f9903
purplesheep513 <72682862+purplesheep513@users.noreply.github.com>
2020-11-16 22:12:12 +0900

    Update ChatContainer.js

--- marrypurple/src/containers/chat/ChatContainer.js
+++ marrypurple/src/containers/chat/ChatContainer.js
@@ -1,44 +1,32 @@
 import React, {useState, useEffect} from 'react';
 import io from 'socket.io-client'
 import '../../lib/styles/chat.scss'
-import $ from 'jquery';
 
 const socket = io.connect('/')
 
 const ChatContainer = ({getChat, loading, user}) => {
     let user_nick ='';
     let profile_pic ='';
-    let _id = '';
+    let user_email = '';
     if(getChat){
         user_nick = getChat.user_nick
         profile_pic = getChat.profile_pic
-        _id = getChat._id
+        user_email = getChat.user_email
     }
-// console.log(_id)
-    const [state, setState] = useState({message: '', name: user_nick});
+console.log('user가 뭐더라,,, 유저는 나였군', user.user_email)
+    const [yourID, setYourID] = useState()
+    const [state, setState] = useState({message: '', name: ''});
     const [chat, setChat] = useState([])
-
+    const {message, name} = state
     
     useEffect(()=>{
-        socket.on('preload',function(data){
-            var output = '';
-            output = `</br><div class="message_2_me"><div class="message_2_me_text">${data.message}</div></div></br>`;
-            $('#msgBoard').append(output);
-        });
-        socket.on('message', ({name, message}) => {
-            setChat([...chat, {name, message}])
+        socket.on('yourId',id =>{
+            setYourID(id)
+        })
+        socket.on('message', (data) => {
+            setChat([...chat, data])
         })
     },[chat])
-
-    const renderChat = () => {
-        return(
-            chat.map(({name, message}, index) => (
-                <div key={index}>
-                    <div>{message}</div>
-                </div>
-            ))
-        )
-    }
 
     const onTextChange = (e) =>{
         setState({...state, [e.target.name]:e.target.value})
@@ -47,11 +35,16 @@ const ChatContainer = ({getChat, loading, user}) => {
     const onMessageSubmit = (e) => {
         e.preventDefault()//계속 새로고침하는것을 막아주는 함수.
         const {name, message} = state
-        socket.emit('sendMessage',{name, message,_id})
+        const output = {
+            id: yourID,
+            sender: user.user_email,
+            recepient: user_email,
+            data: message
+        }
+        socket.emit('sendMessage', output)
         console.log('메세지 보냈습니다.')
         setState({message:'', name})
     }
-
     return(
         <div>
             <div class="message_1">
@@ -60,10 +53,24 @@ const ChatContainer = ({getChat, loading, user}) => {
     <p><span id="message_1_id"></span>{user_nick}님과 매치되었습니다.</p></a>
                 </div>
                 <div class="message_2" id="msgBoard">
-                    <p>2020.10.07</p>
-                    <div class="message_2_you"><div class="message_2_you_photo"></div><div class="message_2_you_text">
-                    </div></div>
-                    {renderChat()}
+                    {/* <p>2020.10.07</p> */}
+                    
+                    {chat.map(({data, id}, index)=>{
+                        if(id === yourID){
+                            return(
+                                <div key={index} class="message_2_me">
+                                    <div class="message_2_me_text"><div>{data}</div></div>
+                                    
+                                </div>
+                            )
+                        }
+                        return(
+                                <div class="message_2_you">
+                                <img id="send_msg_pic" src={profile_pic}/>
+                                <div class="message_2_you_text">{data}</div></div>
+                            )
+                    })}
+                    
                 </div>
                 <div class="message_3">
                     <div id="message_3_emoji"><div></div></div> 

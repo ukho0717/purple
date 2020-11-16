9442a0523b8fae22123b66142a2c580f7a79c0fb
purplesheep513 <72682862+purplesheep513@users.noreply.github.com>
2020-11-16 23:15:42 +0900

    Update ChatContainer.js

--- marrypurple/src/containers/chat/ChatContainer.js
+++ marrypurple/src/containers/chat/ChatContainer.js
@@ -1,6 +1,7 @@
 import React, {useState, useEffect} from 'react';
 import io from 'socket.io-client'
 import '../../lib/styles/chat.scss'
+import $ from 'jquery';
 
 const socket = io.connect('/')
 
@@ -13,12 +14,10 @@ const ChatContainer = ({getChat, loading, user}) => {
         profile_pic = getChat.profile_pic
         user_email = getChat.user_email
     }
-console.log('user가 뭐더라,,, 유저는 나였군', user.user_email)
     const [yourID, setYourID] = useState()
     const [state, setState] = useState({message: '', name: ''});
     const [chat, setChat] = useState([])
-    const {message, name} = state
-    
+    const [preload, setPreload] = useState([])
     useEffect(()=>{
         socket.on('yourId',id =>{
             setYourID(id)
@@ -26,7 +25,15 @@ console.log('user가 뭐더라,,, 유저는 나였군', user.user_email)
         socket.on('message', (data) => {
             setChat([...chat, data])
         })
-    },[chat])
+        socket.on('preloadup',(data)=>{
+            setPreload([...preload,data])
+            console.log('뭐없니ㅣㅣㅣㅣㅣㅣㅣㅣㅣ',data)
+            // var output = '';
+            // output = `</br><div class="message_2_me"><div class="message_2_me_text">${data.message}</div></div></br>`;
+            // $('#msgBoard').append(output);
+
+        });
+    },[chat,preload])
 
     const onTextChange = (e) =>{
         setState({...state, [e.target.name]:e.target.value})
@@ -42,7 +49,7 @@ console.log('user가 뭐더라,,, 유저는 나였군', user.user_email)
             data: message
         }
         socket.emit('sendMessage', output)
-        console.log('메세지 보냈습니다.')
+        // console.log('메세지 보냈습니다.')
         setState({message:'', name})
     }
     return(
@@ -54,7 +61,21 @@ console.log('user가 뭐더라,,, 유저는 나였군', user.user_email)
                 </div>
                 <div class="message_2" id="msgBoard">
                     {/* <p>2020.10.07</p> */}
-                    
+                    {preload.map(({data, sender}, index)=>{
+                        if(sender === user_email){
+                            return(
+                                <div key={index} class="message_2_me">
+                                    <div class="message_2_me_text"><div>뭐없니{data.message}</div></div>
+                                    
+                                </div>
+                            )
+                        }
+                        return(
+                                <div class="message_2_you">
+                                <img id="send_msg_pic" src={profile_pic}/>
+                                <div class="message_2_you_text">{data.message}</div></div>
+                            )
+                    })}
                     {chat.map(({data, id}, index)=>{
                         if(id === yourID){
                             return(

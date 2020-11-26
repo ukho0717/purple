cb8b28248dcde1c974a477ff8a8f6fb873b9fff2
purplesheep513 <72682862+purplesheep513@users.noreply.github.com>
2020-11-26 11:59:02 +0900

    Update index.js

--- server/src/api/matching/index.js
+++ server/src/api/matching/index.js
@@ -9,5 +9,6 @@ match.patch('/super', matchCtrl.sendSuper)      // 슈퍼라이크 눌렀을 때
 match.patch('/pass', matchCtrl.pass);           // 넘기기 눌렀을 때
 match.get('/back', matchCtrl.back);             // (유료) 되돌리기 눌렀을 때
 match.get('/who', matchCtrl.checkLikeMe)        // 나를 라이크 한 사람 보기
+match.post('/report', matchCtrl.report)        // 신고하기
 
 export default match;

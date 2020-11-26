9b0669248c554deea2648aafe2a4bae901db7b6c
wooko <ukho0711@naver.com>
2020-11-26 11:32:44 +0900

    '우코'

--- marrypurple/src/components/auth/Profile.js
+++ marrypurple/src/components/auth/Profile.js
@@ -1,28 +1,13 @@
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> ed61bf815cc78cfe92d3a2be8cd8ef64f918a0a7
 import React, {useEffect, useState} from 'react';
 import '../../lib/styles/profile.scss';
 import { Link } from 'react-router-dom';
 import axios from 'axios';
-<<<<<<< HEAD
-=======
-=======
-// import React, {useState} from 'react';
-// import '../../lib/styles/profile.scss';
->>>>>>> 2cf6053e159fdbe7f9dc9fd45da53964834f02f0
->>>>>>> ed61bf815cc78cfe92d3a2be8cd8ef64f918a0a7
 
 const Profile = ({user,form,onChange,loading,profileSubmit,error, profile}) =>{
  
     const {brief_intro,address,fav_food,fav_movie,fav_song,user_gender,personality,school} = user;
     const elements=[];
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> ed61bf815cc78cfe92d3a2be8cd8ef64f918a0a7
     const [profile_pic, setProfile_pic] = useState(profile);
     
     const [bi, setBi] = useState(brief_intro);
@@ -198,126 +183,25 @@ const Profile = ({user,form,onChange,loading,profileSubmit,error, profile}) =>{
                     <div className="self_intro" id="gender">
                         <p>성별</p> 
                         <p className="input_type_radio">
-=======
-// const Profile = ({user,form,onChange,loading,profileUpdate}) =>{
-//     const {profile_pic,brief_intro,address,fav_food,fav_movie,fav_song,user_gender,school} = user;
-//     const [bi, setBi] = useState(brief_intro,address,fav_food,fav_movie,fav_song,school);
-//     const biChange = e => {
-//         setBi(e.target.value);
-//         console.log(bi)
-//     } 
-//     const clickBack = () => {
-//         window.history.back();
-//     }
-//     return(
-//         <>
-//         {user&&(
-//             <form method="patch" onSubmit={commentSubmit}>
-//             <div className="photo_list">
-//                 <div className="overflow_profile">
-//                     <div>
-//                         <ul className="photo_group">
-//                             <li className="photo_items">
-//                                 <div className="photo_profile"><img src={profile_pic}/></div>
-//                                 <div className="del_btn">
-//                                     <button><i className="fas fa-trash-alt"></i></button>
-//                                 </div>
-//                             </li>
-//                             <li className="photo_items">
-//                                 <div className="photo_profile"></div>
-//                                 <div className="del_btn">
-//                                     <button><i className="fas fa-trash-alt"></i></button>
-//                                 </div>
-//                             </li>
-//                             <li className="photo_items">
-//                                 <div className="photo_profile"></div>
-//                                 <div className="del_btn">
-//                                     <button><i className="fas fa-trash-alt"></i></button>
-//                                 </div>
-//                             </li>
-//                         </ul>
-//                     </div>
-//                     <div>
-//                         <ul className="photo_group">
-//                             <li className="photo_items">
-//                                 <div className="photo_profile"></div>
-//                                 <div className="del_btn">
-//                                     <button><i className="fas fa-trash-alt"></i></button>
-//                                 </div>
-//                             </li>
-//                             <li className="photo_items">
-//                                 <div className="photo_profile"></div>
-//                                 <div className="del_btn">
-//                                     <button><i className="fas fa-trash-alt"></i></button>
-//                                 </div>
-//                             </li>
-//                             <li className="photo_items">
-//                                 <div className="photo_profile"></div>
-//                                 <div className="del_btn">
-//                                     <button><i className="fas fa-trash-alt"></i></button>
-//                                 </div>
-//                             </li>
-//                         </ul>
-//                     </div>
-//                     <div>
-//                         <ul className="photo_group">
-//                             <li className="photo_items">
-//                                 <div className="photo_profile"></div>
-//                                 <div className="del_btn">
-//                                     <button><i className="fas fa-trash-alt"></i></button>
-//                                 </div>
-//                             </li>
-//                             <li className="photo_items">
-//                                 <div className="photo_profile"></div>
-//                                 <div className="del_btn">
-//                                     <button><i className="fas fa-trash-alt"></i></button>
-//                                 </div>
-//                             </li>
-//                             <li className="photo_items">
-//                                 <div className="photo_profile"></div>
-//                                 <div className="del_btn">
-//                                     <button><i className="fas fa-trash-alt"></i></button>
-//                                 </div>
-//                             </li>
-//                         </ul>
-//                     </div>
-//                     <div className="add_photo_btn">
-//                         <p>사진을 추가해 프로필을 변경해보세요</p>
-//                         <button>사진 추가하기</button>
-//                     </div>
-//                     <div className="self_intro" id="intro">
-//                         <p>한줄 소개</p>
-//                         <p className="input_type_text"><input type="text" name="brief_intro" value={bi} onChange={biChange} /></p>
-//                     </div>
-//                     <div className="self_intro" id="live">
-//                         <p>거주지</p> 
-//                         <p className="input_type_text"><input type="text"  name="address" value={address}/></p>
-//                     </div>
-//                 {user.user_gender=="female"&&(
-//                     <div className="self_intro" id="gender">
-//                         <p>성별</p> 
-//                         <p className="input_type_radio">
->>>>>>> 2cf6053e159fdbe7f9dc9fd45da53964834f02f0
 
-//                             <input type="radio" name="user_gender" id="boy" />
-//                             <label for="boy">남</label>
-//                             <input type="radio" name="user_gender" id="girl"checked/>
-//                             <label for="girl">여</label>
+                            <input type="radio" name="user_gender" id="boy" />
+                            <label for="boy">남</label>
+                            <input type="radio" name="user_gender" id="girl"checked/>
+                            <label for="girl">여</label>
 
-//                         </p>
-//                     </div>
-//                         )}
-//                 {user_gender=="male"&&(
-//                     <div className="self_intro" id="gender">
-//                         <p>성별</p> 
-//                         <p className="input_type_radio">
+                        </p>
+                    </div>
+                        )}
+                {user_gender=="male"&&(
+                    <div className="self_intro" id="gender">
+                        <p>성별</p> 
+                        <p className="input_type_radio">
 
-//                             <input type="radio" name="user_gender" id="boy" checked/>
-//                             <label for="boy">남</label>
-//                             <input type="radio" name="user_gender" id="girl"/>
-//                             <label for="girl">여</label>
+                            <input type="radio" name="user_gender" id="boy" checked/>
+                            <label for="boy">남</label>
+                            <input type="radio" name="user_gender" id="girl"/>
+                            <label for="girl">여</label>
 
-<<<<<<< HEAD
                         </p>
                     </div>
                         )}                        
@@ -339,7 +223,6 @@ const Profile = ({user,form,onChange,loading,profileSubmit,error, profile}) =>{
                                 <input type="checkbox" name="personality" id="curious"  value="curious"/><label for="curious">호기심많은</label>
                                 <input type="checkbox" name="personality" id="serious"  value="serious"/><label for="serious">진중한</label>
                                 <input type="checkbox" name="personality" id="agressive"  value="agressive"/><label for="agressive">과감한</label>
-<<<<<<< HEAD
                             </p>
                             <p className="selects">
                                 <input type="checkbox" name="personality" id="intelli"  value="intelli"/><label for="intelli">지적인</label>
@@ -378,46 +261,6 @@ const Profile = ({user,form,onChange,loading,profileSubmit,error, profile}) =>{
                                 <input type="checkbox" className="styles" value="미식가" id="foodlover" value="foodlover" /><label for="foodlover">미식가</label> 
                             </p>
                             <p className="selects">
-=======
-                            </p>
-                            <p className="selects">
-                                <input type="checkbox" name="personality" id="intelli"  value="intelli"/><label for="intelli">지적인</label>
-                                <input type="checkbox" name="personality" id="tsndere"  value="tsndere"/><label for="tsndere">츤데레</label>
-                                <input type="checkbox" name="personality" id="honesty"  value="honesty"/><label for="honesty">솔직한</label>
-                                <input type="checkbox" name="personality" id="growth"  value="growth"/><label for="growth">성숙한</label>
-                            </p>
-                            <p className="selects">
-                                <input type="checkbox" name="personality" id="cool"  value="cool"/><label for="cool">쿨한</label>
-                                <input type="checkbox" name="personality" id="humorous"  value="humorous"/><label for="humorous">유머있는</label>
-                                <input type="checkbox" name="personality" id="careful"  value="careful"/><label for="careful">배려심많은</label>
-                                <input type="checkbox" name="personality" id="timid"  value="timid"/><label for="timid">소심한</label>
-                            </p>
-                            <p className="selects">
-                                <input type="checkbox" name="personality" id="cynical"  value="cynical"/><label for="cynical">시니컬한</label>
-                                <input type="checkbox" name="personality" id="tall"  value="tall"/><label for="tall">키가큰</label>
-                                <input type="checkbox" name="personality" id="small"  value="small"/><label for="small">아담한</label>
-                                <input type="checkbox" name="personality" id="outgoing"  value="outgoing"/><label for="outgoing">쾌활한</label>
-                            </p>
-                            <p className="selects">
-                                <input type="checkbox" className="styles" value="어리버리" id="clumsy" value="clumsy" /><label for="clumsy">어리버리</label>
-                                <input type="checkbox" className="styles" value="감성적" id="emotional" value="emotional" /><label for="emotional">감성적</label>
-                                <input type="checkbox" className="styles" value="책임감있는" id="leadership" value="leadership"  /><label for="leadership">책임감있는</label>
-                                <input type="checkbox" className="styles" value="통통튀는" id="bubbly" value="bubbly"  /><label for="bubbly">통통튀는</label>
-                            </p>
-                            <p className="selects">
-                                <input type="checkbox" className="styles" value="드립장인" id="funny" value="funny"  /><label for="funny">드립장인</label>
-                                <input type="checkbox" className="styles" value="깔끔한" id="tidy" value="tidy"  /><label for="tidy">깔끔한</label>
-                                <input type="checkbox" className="styles" value="힘이센" id="strong" value="strong" /><label for="strong">힘이센</label>
-                                <input type="checkbox" className="styles" value="인정많은" id="empathy"value="empathy"  /><label for="empathy">인정많은</label>
-                            </p>
-                            <p className="selects">
-                                <input type="checkbox" className="styles" value="뚝딱이" id="ddukddak" value="ddukddak"  /><label for="ddukddak">뚝딱이</label>
-                                <input type="checkbox" className="styles" value="사회성좋은" id="social" value="social" /><label for="social">사회성좋은</label>
-                                <input type="checkbox" className="styles" value="믿음직한" id="trustworthy" value="trustworthy"  /><label for="trustworthy">믿음직한</label>
-                                <input type="checkbox" className="styles" value="미식가" id="foodlover" value="foodlover" /><label for="foodlover">미식가</label> 
-                            </p>
-                            <p className="selects">
->>>>>>> ed61bf815cc78cfe92d3a2be8cd8ef64f918a0a7
                                 <input type="checkbox" className="styles" value="활동적인" id="active"  value="active" /><label for="active">활동적인</label>
                                 <input type="checkbox" className="styles" value="고양이파" id="catperson" value="catperson" /><label for="catperson">고양이파</label>
                                 <input type="checkbox" className="styles" value="강아지파" id="dogperson" value="dogperson"  /><label for="dogperson">강아지파</label>
@@ -468,71 +311,5 @@ const Profile = ({user,form,onChange,loading,profileSubmit,error, profile}) =>{
     }
         
 }
-<<<<<<< HEAD
-=======
-=======
-//                         </p>
-//                     </div>
-//                         )}                        
-//                     <div className="self_intro" id="uni">
-//                         <p>출신 학교</p> 
-//                         <p className="input_type_text"><input type="text" value={school} name="school"/></p>
-//                     </div>
-//                     <div>
-//                         <p className="veryStressful">나의 이상형</p>
-//                         <section className="self_intro_check" id="self_intro_check_id">
-//                             <p className="selects">
-//                                 <input type="checkbox" name="personality" id="cute" /><label for="cute">귀여운</label>
-//                                 <input type="checkbox" name="personality" id="mature"/><label for="mature">어른스러운</label>
-//                                 <input type="checkbox" name="personality" id="sexy"/><label for="sexy">섹시한</label>
-//                                 <input type="checkbox" name="personality" id="manly"/><label for="manly">남자다운</label>
-//                             </p>
-//                             <p className="selects">
-//                                 <input type="checkbox" name="personality" id="natural"/><label for="natural">청순한</label>
-//                                 <input type="checkbox" name="personality" id="curious"/><label for="curious">호기심많은</label>
-//                                 <input type="checkbox" name="personality" id="serious"/><label for="serious">진중한</label>
-//                                 <input type="checkbox" name="personality" id="agressive"/><label for="agressive">과감한</label>
-//                             </p>
-//                             <p className="selects">
-//                                 <input type="checkbox" name="personality" id="intelli"/><label for="intelli">지적인</label>
-//                                 <input type="checkbox" name="personality" id="tsndere"/><label for="tsndere">츤데레</label>
-//                                 <input type="checkbox" name="personality" id="honesty"/><label for="honesty">솔직한</label>
-//                                 <input type="checkbox" name="personality" id="growth"/><label for="growth">성숙한</label>
-//                             </p>
-//                             <p className="selects">
-//                                 <input type="checkbox" name="personality" id="cool"/><label for="cool">쿨한</label>
-//                                 <input type="checkbox" name="personality" id="humorous"/><label for="humorous">유머있는</label>
-//                                 <input type="checkbox" name="personality" id="careful"/><label for="careful">배려심많은</label>
-//                                 <input type="checkbox" name="personality" id="timid"/><label for="timid">소심한</label>
-//                             </p>
-//                             <p className="selects">
-//                                 <input type="checkbox" name="personality" id="cynical"/><label for="cynical">시니컬한</label>
-//                                 <input type="checkbox" name="personality" id="tall"/><label for="tall">키가큰</label>
-//                                 <input type="checkbox" name="personality" id="small"/><label for="small">아담한</label>
-//                                 <input type="checkbox" name="personality" id="outgoing"/><label for="outgoing">쾌활한</label>
-//                             </p>
-//                         </section>
-//                     </div>
-//                     <div className="self_intro" id="best_song">
-//                         <p>SPOTIFY 베스트송</p> 
-//                         <p className="input_type_text"><input type="text" value={fav_song} name="fav_song"/></p>
-//                     </div>
-//                     <div className="self_intro" id="fav_movie">
-//                         <p>좋아하는 영화장르</p> 
-//                         <p className="input_type_text"><input type="text" value={fav_movie} name="fav_movie"/></p>
-//                     </div>
-//                     <div className="self_intro" id="food">
-//                         <p>음식취향</p> 
-//                         <p className="input_type_text"><input type="text" value={fav_food} name="fav_food"/></p>
-//                     </div>
-//                     <div className="submit_btn"><input type="submit" value="수정하기"/></div>
-//                 </div>
-//                 </div>
-//             </form>
-//             )}
-//         </>
-//             )}
->>>>>>> 2cf6053e159fdbe7f9dc9fd45da53964834f02f0
->>>>>>> ed61bf815cc78cfe92d3a2be8cd8ef64f918a0a7
         
-// export default Profile;
+export default Profile;
--- marrypurple/src/components/auth/find_pw.js
+++ marrypurple/src/components/auth/find_pw.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React from 'react';
 import { Link} from 'react-router-dom';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
@@ -64,54 +63,3 @@ const FindPw = ({type,form,onChange,onSubmit,error}) => {
     )
 }
 export default FindPw;
-=======
-// import React from 'react';
-// import { Link } from 'react-router-dom';
-// import '../../lib/styles/regist.scss';
-// import rLogo from '../../lib/img/rLogo.png';
-
-// const FindPw = ({type,form,onChange,onSubmit,error}) => {
-//     return(
-//         <div id="wrapM">
-//         <div id="container">
-//             <div id="Rheader">
-//                     <a><Link to="/" class="rLogo"><img src={rLogo} alt="회원가입로고"/></Link></a>
-//             </div>
-//             <div class="rContainer">
-//                 <div class="regiT findPw">
-//                     <h1>비밀번호 찾기</h1>
-//                 </div>
-//             <form onSubmit={onSubmit}>
-//                 <div class="regiId">
-//                     <p><span class="idText">아이디</span></p>
-//                     <div>
-//                         <div class="inputId">
-//                             <input  
-//                                 class="flexS" 
-//                                 type="text" 
-//                                 name="userId"
-//                                 placeholder="이메일 주소(아이디)"
-//                                 onChange={onChange}
-//                                 value={form.user_email} 
-//                             />
-//                         </div>
-//                         <input class="getE" type="button" name="idCheck" value="인증 받기"/>
-//                     </div>
-//                 </div> 
-//                 <div class="regiId">
-//                     <div class="inputId">
-//                         <input class="reNum" type="text" name="chkNum" placeholder="인증번호"/>
-//                     </div>
-//                     <input class="getE" type="button" name="Check" value="인증"/>
-//                 </div>
-//                 <div class="reBtn">
-//                     <a><Link to="/changePw"><input class="rePw" type="button" name="rePw1" value="변경하기" onClick={onClick}/></Link></a>
-//                 </div>
-//             </form>
-//             </div>
-//         </div>
-//     </div>
-//     )
-// }
-// export default FindPw;
->>>>>>> 2cf6053e159fdbe7f9dc9fd45da53964834f02f0
